e78b5cb52d4c5e0419a72b7f9fdbf8e8416a8eba
Herman <german.makarkin@gmail.com>
2025-01-10 22:26:09 +0300

    glitchtip

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,9 @@
+use Sentry\Laravel\Integration;
+
+public function register(): void
+{
+    $this->reportable(function (Throwable $e) {
+        Integration::captureUnhandledException($e);
+    });
+}
+
--- routes/web.php
+++ routes/web.php
@@ -35,4 +35,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/error', function () {
+    throw new Exception('My first GlitchTip error!');
+});
+
 require __DIR__.'/auth.php';
